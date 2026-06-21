5ca0e2039305c099de17c81ec7e60dd28c97c291
Gavstrav <Gavstrav@outlook.com>
2026-06-21 01:43:15 +0100

    Add scripts

---
+++ TwitchAuthListener.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using TwitchLib.Api;
+
+namespace TwitchOAuthListener
+{
+    public class TwitchAuthListener
+    {
+        private readonly string _clientId;
+        private readonly string _redirectUri;
+        private readonly string _authUrl;
+		private const string OAUTH_HTML = @"
+		<!DOCTYPE html>
+		<html lang='en'>
+		<head>
+				<meta charset='UTF-8' />
+				<title>Twitch OAuth</title>
+		</head>
+		<body>
+				<h2>Authorization Successful</h2>
+				<p>You can close this window.</p>
+
+				<script>
+						var accessToken = window.location.hash.split('&')[0].split('=')[1];
+
+						var xhr = new XMLHttpRequest();
+						xhr.open('POST', 'http://localhost:3000', true);
+						xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+						xhr.onreadystatechange = function () {
+								if (xhr.readyState === 4 && xhr.status === 200) {
+										window.close();
+								}
+						};
+
+						xhr.send('access_token=' + accessToken);
+
+						window.history.replaceState(null, null, window.location.pathname);
+				</script>
+		</body>
+		</html>";
+
+        private HttpListener _listener;
+
+        public event Action<TwitchAPI, TwitchBot> OnConnected;
+
+        public TwitchAuthListener(string clientId, string redirectUri = "http://localhost:3000")
+        {
+            _clientId = clientId;
+            _redirectUri = redirectUri;
+
+            _authUrl =
+                $"https://id.twitch.tv/oauth2/authorize" +
+                $"?response_type=token" +
+                $"&client_id={clientId}" +
+                $"&redirect_uri={Uri.EscapeDataString(redirectUri)}" +
+                $"&scope=chat:read+chat:edit";
+        }
+
+        public void Start()
+        {
+            _listener?.Stop();
+
+            _listener = new HttpListener();
+            _listener.Prefixes.Add(_redirectUri + "/");
+            _listener.Start();
+
+            Task.Run(async () =>
+            {
+                while (_listener.IsListening)
+                {
+                    var context = await _listener.GetContextAsync();
+
+                    if (context.Request.HttpMethod == "GET")
+                        HandleGet(context);
+                    else if (context.Request.HttpMethod == "POST")
+                        HandlePost(context);
+                }
+            });
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = _authUrl,
+                UseShellExecute = true
+            });
+        }
+
+        private void HandleGet(HttpListenerContext context)
+        {
+            var buffer = Encoding.UTF8.GetBytes(OAUTH_HTML);
+
+            context.Response.ContentType = "text/html";
+            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            context.Response.Close();
+        }
+
+        private void HandlePost(HttpListenerContext context)
+        {
+            string body;
+
+            using (var reader = new StreamReader(context.Request.InputStream))
+                body = reader.ReadToEnd();
+
+            var parameters = HttpUtility.ParseQueryString(body);
+            var accessToken = parameters["access_token"];
+
+            context.Response.StatusCode = 200;
+            context.Response.Close();
+
+            if (!string.IsNullOrEmpty(accessToken))
+                _ = SetupTwitch(accessToken);
+        }
+
+        private async Task SetupTwitch(string accessToken)
+        {
+            var api = new TwitchAPI
+            {
+                Settings =
+                {
+                    ClientId = _clientId,
+                    AccessToken = accessToken
+                }
+            };
+
+            var user = await api.Helix.Users.GetUsersAsync();
+
+            if (user?.Users.Length > 0)
+            {
+                var login = user.Users[0].Login;
+
+                var bot = new TwitchBot(login, accessToken);
+
+                OnConnected?.Invoke(api, bot);
+
+                _listener?.Stop();
+            }
+        }
+    }
+}
---
+++ TwitchBot.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace TwitchOAuthListener
+{
+    public class TwitchBot
+    {
+        private readonly TwitchClient _client;
+        private readonly string _channel;
+
+        public bool IsConnected { get; private set; }
+
+        public event Action OnDisconnected;
+        public event Action<ChatMessage> OnJoinCommandReceived;
+        public event Action<string> OnUserBanned;
+        public event Action<string> OnUserTimedOut;
+
+        public TwitchBot(string username, string accessToken)
+        {
+            var credentials = new ConnectionCredentials(username, accessToken);
+
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+            };
+
+            _channel = username;
+
+            var webSocketClient = new WebSocketClient(clientOptions);
+            _client = new TwitchClient(webSocketClient);
+
+            _client.Initialize(credentials, _channel);
+
+            _client.OnConnected += (_, _) => IsConnected = true;
+            _client.OnDisconnected += (_, _) =>
+            {
+                IsConnected = false;
+                OnDisconnected?.Invoke();
+            };
+
+            _client.OnChatCommandReceived += HandleChatCommand;
+            _client.OnUserBanned += (_, e) => OnUserBanned?.Invoke(e.UserBan.Username);
+            _client.OnUserTimedout += (_, e) => OnUserTimedOut?.Invoke(e.UserTimeout.Username);
+
+            _client.Connect();
+        }
+
+        private void HandleChatCommand(object sender, OnChatCommandReceivedArgs e)
+        {
+            if (Regex.IsMatch(e.Command.CommandText, @"join[:\s]*([a-zA-Z,]*)", RegexOptions.IgnoreCase))
+            {
+                OnJoinCommandReceived?.Invoke(e.Command.ChatMessage);
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            if (_client.IsConnected)
+                _client.SendMessage(_channel, message);
+        }
+
+        public void Disconnect()
+        {
+            if (_client.IsConnected)
+                _client.Disconnect();
+        }
+    }
+}
